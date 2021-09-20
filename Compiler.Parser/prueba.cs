43643ef0e3c77b33993614910050419689f29445
Mael2001 <Mariof2001@unitec.edu>
2021-09-19 22:17:50 -0600

    Push De nuevo

--- Compiler.Parser/prueba.cs
+++ Compiler.Parser/prueba.cs
@@ -3,42 +3,35 @@ using System.Collections.Generic;
 
 namespace Compiler.Parser
 {
-    public class prueba
+    class bruh
     {
-        private void plebe()
+
+        void prueba()
         {
             int w;
             float o;
-            int x= 0;
-            List<int> ps = new List<int>
-            {
-                new int(),
-                new int()
-            };
-            int xx =20;
-            int y = 10;
+            int x =2;
+            int xx=1;
+            int y=1;
             DateTime datex;
             string a;
             o = 5.89F;
-            datex = new DateTime(09/12/2021);
+            datex = new DateTime(09 / 12 / 2021);
             x++;
             x--;
-            a = o + "hola peter!:";
-            foreach (var asd in ps)
-            {
-
-            }
+            a = o + "hola peter!";
+            //foreach (x in xx)
             x++;
             while (o >= 10 || x > 12)
                 x++;
 
             if (x > y && y > 10 || xx > 50 && xx < 100)
-            {
-                x = 10;
-            }
+                x= 10;
             else
                 a= "adios";
-                y=6+2+3+5;
+            y= 6 + 2 + 3 + 5;
+
         }
     }
+
 }
